bbf0a43fe9b9e877ca6c62799e5d8b6b0e0a0fd2
Albert Zhao <zhaoq@kean.edu>
2023-11-27 20:37:05 +0800

    change all the files to java project file, which is more efficient to write codes, run, and debug

---
+++ src/boat/Boat.java
@@ -0,0 +1,27 @@
+package boat;
+public class Boat {
+    public String make;
+    public String varient;
+    public int length;
+    public String region;
+    public int borrowPrice;
+    public int sellPrice;
+    public int year;
+
+    public Boat(String make, String varient, int length, String region, int sellPrice, int year) {
+        this.make = make;
+        this.varient = varient;
+        this.length = length;
+        this.region = region;
+        this.sellPrice = sellPrice;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getVarient() {
+        return varient;
+    }
+}
---
+++ src/boat/BoatCreator.java
@@ -0,0 +1,41 @@
+package boat;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoatCreator {
+    public static void main(String[] args) {
+        String csvFile = "resources/2023_MCM_Problem_Y_Boats.xlsx";
+        String line;
+        String csvSplitBy = ",";
+        List<Boat> boats = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            br.readLine();
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                line = line.replace("\"", "");
+                String[] data = line.split(csvSplitBy);
+                String make = data[0];
+                String variant = data[1];
+                int length = Integer.parseInt(data[2]);
+                String region = data[3];
+                String price = data[5].replaceAll("[^\\d.]", ""); // Remove non-numeric characters from price
+                int sellPrice = Integer.parseInt(price);
+                int year = Integer.parseInt(data[6]);
+                // Create a new Boat object using the constructor
+                Boat boat = new Boat(make, variant, length, region, sellPrice, year);
+                boats.add(boat);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Number of boats created: " + boats.size());
+        System.out.println(boats.get(1000).getMake());
+    }
+}
