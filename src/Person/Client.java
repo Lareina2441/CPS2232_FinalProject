bcae47c5a626d60177e7051d0d939659e68eeb19
Albert Zhao <zhaoq@kean.edu>
2023-11-27 23:00:26 +0800

    Client class changed

---
+++ src/Person/Client.java
@@ -0,0 +1,33 @@
+package Person;
+public class Client {
+    private int priority;
+    private String name;
+    private int password;
+
+    public Client(int priority, String name, int password) {
+        this.priority = priority;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "priority=" + priority +
+                ", name='" + name + '\'' +
+                ", password=" + password +
+                '}';
+    }
+}
---
+++ src/csv/ClientCsvCreator.java
@@ -0,0 +1,31 @@
+package csv;
+
+import Person.Client;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ClientCsvCreator {
+    public static void main(String[] args) {
+        List<Client> clients = new ArrayList<>();
+        clients.add(new Client(1, "Alice", 1234));
+        clients.add(new Client(2, "Bob", 5678));
+        clients.add(new Client(3, "Charlie", 9876));
+
+        String csvFilePath = "E:\\note\\clients.csv";
+
+        writeClientsToCSV(clients, csvFilePath);
+    }
+
+    public static void writeClientsToCSV(List<Client> clients, String filePath) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))) {
+            for (Client client : clients) {
+                writer.println(client.getPriority() + "," + client.getName() + "," + client.getPassword());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
