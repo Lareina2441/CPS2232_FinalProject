11634af38f3d29915f0bddc1664d81074d7f79af
AlbertZhao <111401870+AlbertZhaoCA@users.noreply.github.com>
2023-12-01 01:46:28 +0800

    data structure constructed !!!!

--- src/backend/Database.java
+++ src/backend/Database.java
@@ -1,45 +1,50 @@
 package backend;
 
 import boat.Boat;
-import boat.BoatCreator;
 import person.Sailor;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Queue;
 import java.util.TreeMap;
 
 public class Database {
+    //do operations based on the attribute
     HashMap<String,ArrayList<Boat>> byAttributeBoats = new HashMap<>();
-    TreeMap<Integer,Boat> byPriceBoats = new TreeMap(Boat.getComparator());
-    BoatCreator boatCreator = new BoatCreator();
+
+    //use price as default "comparator to sort"
+    TreeMap<Boat,Boat> byPriceBoats = new TreeMap<>();
 
     ArrayList<Sailor> sailors = new ArrayList<>();
 
     public static void main(String[] args) {
         Database database = new Database();
-        System.out.println(database.byAttributeBoats.get("Bavaria"));
-        System.out.println(database.byPriceBoats);
+
     }
 
     public Database(){
-        boatCreator.createBoats();
-        ArrayList <Boat> boats = new ArrayList<>();
+        //testing 
+        Boat[] boats = new Boat[3];
+        boats[0] = new Boat("Bav", "Cruiser 46", 14, "Mediterranean", 39999990, 200, 10, 20124);
+        boats[1] = new Boat("Alamba", "Cruiser 56", 234, "Mediterraneaner", 30000, 2001000, 100400, 20214);
+        boats[2] = new Boat("Bavaria", "Cruiser 46", 24, "Mediterraneean", 3000, 2000300, 100100, 20414);
+
         //use different attribute to file the database
-        for (Boat boat : boatCreator.boats
+        for (Boat boat : boats
         ) {
             byAttributeBoats.computeIfAbsent(boat.getMake(), k -> new ArrayList<>()).add(boat);//by MAKE
         }
-        for (Boat boat : boatCreator.boats
+        for (Boat boat : boats
         ) {
             byAttributeBoats.computeIfAbsent(boat.getRegion(), k -> new ArrayList<>()).add(boat);//by REGION
         }
-        for (Boat boat : boatCreator.boats
+        for (Boat boat : boats
         ) {
             byAttributeBoats.computeIfAbsent(boat.getVarient(), k -> new ArrayList<>()).add(boat);//by VARIANT
         }
-        for (Boat boat : boatCreator.boats
+        for (Boat boat : boats
         ) {
-            byPriceBoats.put(boat.getSellPrice(),boat);
+            price.put(boat,boat);//by PRICE
         }
+
     }
 }
