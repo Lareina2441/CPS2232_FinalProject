9cb18ca9c7f0cd86090105127d16e24ab96e33a7
Albert Zhao <zhaoq@kean.edu>
2023-12-01 00:14:11 +0800

    Experiment

--- src/backend/Database.java
+++ src/backend/Database.java
@@ -1,18 +1,45 @@
 package backend;
 
 import boat.Boat;
-
+import boat.BoatCreator;
 import person.Sailor;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Queue;
+import java.util.TreeMap;
 
 public class Database {
-    HashMap<> boats = new HashMap<>();
-    HashMap<> clients = new HashMap<>();
+    HashMap<String,ArrayList<Boat>> byAttributeBoats = new HashMap<>();
+    TreeMap<Integer,Boat> byPriceBoats = new TreeMap(Boat.getComparator());
+    BoatCreator boatCreator = new BoatCreator();
 
     ArrayList<Sailor> sailors = new ArrayList<>();
-    Queue<Client> clientQueue;
 
+    public static void main(String[] args) {
+        Database database = new Database();
+        System.out.println(database.byAttributeBoats.get("Bavaria"));
+        System.out.println(database.byPriceBoats);
+    }
+
+    public Database(){
+        boatCreator.createBoats();
+        ArrayList <Boat> boats = new ArrayList<>();
+        //use different attribute to file the database
+        for (Boat boat : boatCreator.boats
+        ) {
+            byAttributeBoats.computeIfAbsent(boat.getMake(), k -> new ArrayList<>()).add(boat);//by MAKE
+        }
+        for (Boat boat : boatCreator.boats
+        ) {
+            byAttributeBoats.computeIfAbsent(boat.getRegion(), k -> new ArrayList<>()).add(boat);//by REGION
+        }
+        for (Boat boat : boatCreator.boats
+        ) {
+            byAttributeBoats.computeIfAbsent(boat.getVarient(), k -> new ArrayList<>()).add(boat);//by VARIANT
+        }
+        for (Boat boat : boatCreator.boats
+        ) {
+            byPriceBoats.put(boat.getSellPrice(),boat);
+        }
+    }
 }
