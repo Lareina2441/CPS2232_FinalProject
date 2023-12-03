3a00927b5ea853aa310f9f5ecb6c09a4eb5b66e0
GemmaJiao <152167380+GemmaJiao@users.noreply.github.com>
2023-12-03 15:20:11 +0800

    Update BoatCreator.java

--- src/boat/BoatCreator.java
+++ src/boat/BoatCreator.java
@@ -1,14 +1,12 @@
-package boat;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+package CPS2232.FinalProject;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+  //Create boat array
 
-public class BoatCreator {
+public class BoatCreator implements Serializable {
     public static void main(String[] args) {
-        String csvFile = "resources/2023_MCM_Problem_Y_Boats.xlsx";
+        String csvFile = "D:\\MCMTraining\\dataFolder\\2023_MCM_Problem_Y_Boats.csv";
         String line;
         String csvSplitBy = ",";
         List<Boat> boats = new ArrayList<>();
@@ -20,24 +18,36 @@ public class BoatCreator {
             while ((line = br.readLine()) != null) {
                 line = line.replace("\"", "");
                 String[] data = line.split(csvSplitBy);
-                String maker = data[0];
+                String make = data[0];
                 String variant = data[1];
                 int length = Integer.parseInt(data[2]);
                 String region = data[3];
                 String price = data[5].replaceAll("[^\\d.]", ""); // Remove non-numeric characters from price
-                int sellPrice = Integer.parseInt(price);
+                int costPrice = Integer.parseInt(price);
                 int year = Integer.parseInt(data[6]);
-                int costPrice = sellPrice - 100 - length * 2 - year / 10;
-                int rentPrice = sellPrice / 100;
+                double sellPrice = costPrice * (30 + year * 20) / 30000;
+                double rentPrice = sellPrice / 200;
                 // Create a new Boat object using the constructor
-                Boat boat = new Boat(maker, variant, length, region, sellPrice, costPrice,sellPrice, year);
+                Boat boat = new Boat(make, variant, length, region, costPrice, sellPrice, rentPrice, year);
                 boats.add(boat);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        saveBoatsToFile(boats);
         System.out.println("Number of boats created: " + boats.size());
         System.out.println(boats.get(1000).getMake());
     }
+
+    private static void saveBoatsToFile(List<Boat> boats) {
+        String filePath = "E:\\SessionsAbout2023Fall\\CPS2232\\FinalProject\\Dataset\\allBoat";
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(boats);
+            System.out.println("Boats list has been saved to file: " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
