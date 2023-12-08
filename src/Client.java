ad075920695376868199c40bf9c57d777c000939
GemmaJiao <152167380+GemmaJiao@users.noreply.github.com>
2023-12-08 21:01:53 +0800

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,10 @@
-package calculator.A.FInal;
+package CPS2232.FinalProject;
 
-import java.util.Comparator;
+import java.io.Serializable;
 import java.util.HashSet;
 
-public class Client{
+public class Client implements Person, Serializable {
+
     //  This class describes the attributes and operations of clients.
     private String name;
     private String password;
@@ -15,27 +16,53 @@ public class Client{
     //  The own is the set of boats that the client has bought.
     private HashSet<Boat> own = new HashSet<Boat>();
 
-    Comparator<Client> comparator = new Comparator<Client>() {
+/*    Comparator<Client> comparator = new Comparator<Client>() {
         @Override
         public int compare(Client c1, Client c2) {
             return c1.credit().compareTo(c2.credit());
         }
-    };
+    };*/
+    private String uniqueID;
 
     public Client(String name, String password, String email) {
         this.name = name;
         this.password = password;
         this.email = email;
-        this.credit = 0;
-
-    }
-    public String getEmail(String name) {
-        return email;
     }
 
+    public String getUniqueID() {
+        return uniqueID;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-    
+    /**
+     *
+     */
+    @Override
+    public void setName() {
+        this.name = name;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void setPassword() {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                ", name='" + name + '\'' +
+                ", password=" + password +
+                '}';
+    }
 }
-
